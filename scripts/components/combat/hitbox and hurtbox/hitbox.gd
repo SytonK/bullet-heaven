39858ccdb2d9e6a9ddacf528f098c20ca123654e
yoni <syonik98@gmail.com>
2026-03-06 13:06:08 +0200

    ref/hitbox-and-hurtbox

--- scripts/components/combat/hitbox and hurtbox/hitbox.gd
+++ scripts/components/combat/hitbox and hurtbox/hitbox.gd
@@ -3,4 +3,10 @@ class_name Hitbox extends Area2D
 @export var attack_resource: AttackResource
 
 func _init() -> void:
-	collision_mask = 0
+	collision_layer = 0
+	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(area: Area2D) -> void:
+	assert(area is Hurtbox, "area entered hurtbox that is not a hitbox")
+	
+	(area as Hurtbox).get_attacked(attack_resource)
--- scripts/components/combat/hitbox and hurtbox/hurtbox.gd
+++ scripts/components/combat/hitbox and hurtbox/hurtbox.gd
@@ -3,11 +3,7 @@ class_name Hurtbox extends Area2D
 signal hurt(attack_resource: AttackResource)
 
 func _init() -> void:
-	area_entered.connect(_on_area_entered)
+	collision_mask = 0
 
 func get_attacked(attack_resource: AttackResource) -> void:
 	hurt.emit(attack_resource)
-
-func _on_area_entered(area: Area2D) -> void:
-	assert(area is Hitbox, "area entered hurtbox that is not a hitbox")
-	get_attacked((area as Hitbox).attack_resource)
